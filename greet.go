9d14431ae19a7e17162d2751c4b12a6a5a3ccd10
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-02 18:30:56 -0500

    Initial Commit

---
+++ greet.go
@@ -0,0 +1,13 @@
+package greet
+
+var emoji = "🎩"
+
+// English saludo en ingles
+func English() string {
+	return "Hi " + emoji
+}
+
+// Italian retorna saludo en italiano
+func Italian() string {
+	return "Ciao " + emoji
+}
