b03d891c18975c7fa458ca17eb0ee06060d94b09
sebastianterleira <seb.terleira1204@gmail.com>
2023-02-02 22:05:41 -0500

    Impotar paquetes de terceros

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/sebastianterleira/Hello-paquetes-GO/greet"
+	"rsc.io/quote"
 )
 
 func main() {
 	fmt.Println(greet.Spanish())
+	fmt.Println(quote.Hello())
 }
